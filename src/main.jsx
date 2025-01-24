c61d9a72a77f4e56f15abb8e5c1682aeb71a5b43
JoyousUser <jenaounisamiboite@gmail.com>
2025-01-24 13:18:51 +0100

    logging and imports; host bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import { AppProvider } from '../src/appContext';
 import apiBaseUrl from './config';
 import api from './services/api'
+console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
 
 
 const rootElement = document.getElementById('root');
